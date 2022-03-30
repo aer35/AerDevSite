9a6f6aa2a816c77fe913a0f44ea3fa6d0e8aeb2f
Aaron <aer35@users.noreply.github.com>
2022-03-29 22:45:34 -0400

    Darkmode toggle working

--- App.tsx
+++ App.tsx
@@ -9,45 +9,39 @@ import {
   Card,
   Switch,
 } from "@nextui-org/react";
-import * as DarkReader from "darkreader";
-
-//TODO add darkreader state for switch state 
+import DarkModeSwitch from "./components/DarkModeSwitch";
 
 export default function App() {
-  useEffect(() => {
-    DarkReader.auto({});
-  }, []);
-
   return (
-    <Container>
+    <>
       <Container>
-        <Switch
-          checked={DarkReader.isEnabled()}
-          onChange={() =>
-            DarkReader.isEnabled() ? DarkReader.disable() : DarkReader.auto({})
-          }
-        />
+        {/* Sticky header goes here */}
+        <DarkModeSwitch />
       </Container>
-      <Card>HELLO</Card>
-      <Text
-        h3
-        css={{
-          textGradient: "45deg, $blue500 -20%, $pink500 50%",
-        }}
-      >
-        Hello World 1
-      </Text>
-      <Spacer />
+
       <Container>
+        <Container></Container>
+        <Card>HELLO</Card>
         <Text
-          h6
+          h3
           css={{
-            textGradient: "0deg, #000000 -10%, red 100%",
+            textGradient: "45deg, $blue500 -20%, $pink500 50%",
           }}
         >
-          Hello World 2
+          Hello World 1
         </Text>
+        <Spacer />
+        <Container>
+          <Text
+            h6
+            css={{
+              textGradient: "0deg, #000000 -10%, red 100%",
+            }}
+          >
+            Hello World 2
+          </Text>
+        </Container>
       </Container>
-    </Container>
+    </>
   );
 }
--- components/DarkModeSwitch.tsx
+++ components/DarkModeSwitch.tsx
@@ -1,20 +1,30 @@
-import { Switch, changeTheme, useTheme } from "@nextui-org/react";
+import { Switch } from "@nextui-org/react";
+import * as DarkReader from "darkreader";
+import { useState, useEffect } from "react";
 
 const DarkModeSwitch = () => {
-  const { type, isDark } = useTheme();
+  const [isDark, setDark] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    if (isDark) {
+      DarkReader.auto({});
+    }
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) => {
+        setDark(!!e.matches);
+        !e.matches ? DarkReader.disable() : DarkReader.auto({});
+      });
+  }, []);
 
   const handleChange = () => {
-    const nextTheme = isDark ? "light" : "dark";
-    window.localStorage.setItem("data-theme", nextTheme); // you can use any storage
-    changeTheme(nextTheme);
+    isDark ? DarkReader.disable() : DarkReader.auto({});
+    setDark(!isDark);
   };
 
-  return (
-    <div>
-      The current theme is: {type}
-      <Switch checked={isDark} onChange={handleChange} />
-    </div>
-  );
+  return <Switch checked={isDark} onChange={handleChange} />;
 };
 
 export default DarkModeSwitch;
