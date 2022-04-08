f0c5e01c09dc18e94fcdf86257e32ae7385cffd2
Aaron <aer35@users.noreply.github.com>
2022-04-07 21:08:16 -0400

    Next.Js conversion for page routing implementation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,33 @@
 import { Link } from "@nextui-org/react";
 import DarkModeSwitch from "./DarkModeSwitch";
-import "../styles/Header.scss";
+import styles from "../styles/Header.module.scss";
+import { useEffect } from "react";
+import * as DarkReader from "darkreader";
 
-const Header = () => (
-  <div className={"header"} id="header">
-    <div className="header__links">
-      <Link href="" className="header__name" block color="primary">
-        Aaron Mechanic
-      </Link>
+const Header = () => {
+  useEffect(() => {
+    DarkReader.setFetchMethod(window.fetch);
+  }, []);
 
-      <Link href="#" block underline color="primary">
-        Resume
-      </Link>
-      <Link href="#" block underline color="primary">
-        Projects
-      </Link>
+  return (
+    <div className={styles.header} id="header">
+      <div className={styles.header__links}>
+        <Link href="" className={styles.header__name} block color="primary">
+          Aaron Mechanic
+        </Link>
+
+        <Link href="#" block underline color="primary">
+          Resume
+        </Link>
+        <Link href="#" block underline color="primary">
+          Projects
+        </Link>
+      </div>
+      <div className={styles.darkModeSwitch}>
+        <DarkModeSwitch />
+      </div>
     </div>
-    <div className="darkModeSwitch">
-      <DarkModeSwitch />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/main.scss";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
