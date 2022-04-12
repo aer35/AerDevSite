2b003f516c5170282260588fbb7fe22bf45d75ab
Aaron <aer35@users.noreply.github.com>
2022-04-12 18:06:19 -0400

    Someone made an oopsie
    Changed a class name because apparently im not very creative.

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -21,12 +21,35 @@ const AboutMe = () => (
       css={{ size: "$72" }}
       zoomed
     />
-    {/* TODO Choose new image */}
-    <Spacer y={1.5} />
+    <Spacer />
+    <Text
+      h1
+      css={{
+        textGradient: "30deg, var(--primary) -20%, $red500 150%",
+      }}
+      weight="extrabold"
+    >
+      Aaron Mechanic
+    </Text>
+    <Text h3>B.S. Computer Science - CUNY Queens college 2021</Text>
+    <Spacer />
+    <Container className="bio">
+      <Text>
+        Hi! I{"'"}m Aaron, a software engineer from in New York City and I{"'"}
+        ve been a computer fanatic my whole life. I fell in love with computers
+        ever since my first, an iMac G3. I love learning new things and
+        improving my skills which is why I decided to become a programmer! Every
+        week 300000 new Javascript frameworks are released and I learn them all.
+        Haha just kidding. Recently I{"'"}ve been working a lot with React
+        Typescript, nextJS, Node, and Swift.
+      </Text>
+    </Container>
+    {/* Codify the text and move the CSS for social links and about me to a module */}
+    <Spacer y={2} />
     <Container
       className="social_links"
       display="flex"
-      justify="center"
+      justify="space-evenly"
       alignItems="center"
       alignContent="center"
     >
@@ -52,31 +75,6 @@ const AboutMe = () => (
         <FontAwesomeIcon size="4x" icon={faAngellist} />
       </a>
     </Container>
-    <Spacer />
-    <Text
-      h1
-      css={{
-        textGradient: "30deg, var(--primary) -20%, $red500 150%",
-      }}
-      weight="extrabold"
-    >
-      Aaron Mechanic
-    </Text>
-    <Text h3>B.S. Computer Science - CUNY Queens college 2021</Text>
-    <Spacer />
-    <Container className="aboutMe">
-      <Text>
-        Hi! I{"'"}m Aaron, a software engineer from in New York City and I{"'"}
-        ve been a computer fanatic my whole life. I fell in love with computers
-        ever since my first, an iMac G3. I love learning new things and
-        improving my skills which is why I decided to become a programmer! Every
-        week 300000 new Javascript frameworks are released and I learn them all.
-        Haha just kidding. Recently I{"'"}ve been working a lot with React
-        Typescript, nextJS, Node, and Swift.
-      </Text>
-    </Container>
-    {/* Codify the text and move the CSS for social links and about me to a module */}
-    <div style={{ height: "500px" }} />
   </Container>
 );
 
