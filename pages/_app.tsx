df39bcb700fde2ae2fbd7910aa5efc72b6387639
Aaron <aer35@users.noreply.github.com>
2024-01-18 13:53:18 -0500

    move old branch changes over

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,30 @@
 import "../styles/main.scss";
-import type { AppProps } from "next/app";
-import { NextUIProvider } from "@nextui-org/react";
-import { config } from "@fortawesome/fontawesome-svg-core";
+import type {AppProps} from "next/app";
+import {NextUIProvider} from "@nextui-org/react";
+import {config} from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Head from "next/head";
-import { ToastContainer } from "react-toastify";
+import {ToastContainer} from "react-toastify";
+
 config.autoAddCss = false;
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <NextUIProvider>
-      <Head>
-        <title>AerDevSite</title>
-        <meta
-          name="description"
-          content="Website designed and developed by Aaron Mechanic"
-        />
-        <link rel="icon" href="/images/AerDevIcon3_500.png" />
-      </Head>
-      <Header />
-      <ToastContainer />
-      <Component {...pageProps} />
-      <Footer />
-    </NextUIProvider>
-  );
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <NextUIProvider>
+            <Head>
+                <title>AerDevSite</title>
+                <meta
+                    name="description"
+                    content="Website designed and developed by Aaron Mechanic"
+                />
+                <link rel="icon" href="/images/AerDevIcon3_500.png"/>
+            </Head>
+            <ToastContainer/>
+            <Component {...pageProps} />
+            <Footer/>
+        </NextUIProvider>
+    );
 }
 
 export default MyApp;
